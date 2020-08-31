5342df52ef571bca8722ce161f901c387b557800
Do Chanh Tin <@gmail>
2020-08-31 08:48:54 +0700

    Update

---
+++ Design Patterns/TH-TrienKhaiObjectPool/src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+
+public class Main {
+        public static void main(String[] args) {
+            JDBCConnectionPool pool = new JDBCConnectionPool(
+                    "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+                    "sa", "secret");
+
+            // Get a connection:
+            Connection con = pool.checkOut();
+
+            pool.checkIn(con);
+
+        }
+}
+
